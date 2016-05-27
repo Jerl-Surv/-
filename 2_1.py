4a324f5add6b8417c1674b43e625a04755b26fa1
Jerl-Surv <duflot.julia@gmail.com>
2016-05-27 04:15:36 +0300

    хз

--- 2_1.py
+++ 2_1.py
@@ -10,6 +10,7 @@ c = []
 const = [1, 2, 1, 1.5]
 tau = [0.001, 0.01, 0.1, 0.3, 0.5, 0.7]
 K = []
+K_cash = []
 y1 = [1]
 y2 = [0.05]
 t = [0]
@@ -74,10 +75,13 @@ def sum1(st, en):
 		res = res + float(b[i][en-1])*(const[0] - const[1]*K[i][2])*K[i][1]
 	return res
 
-def sum1_im(num, st, en):
+def sum1_im(st):
 	res = 0.0
-	for i in range(st, en):
-		res = res + float(b[i][en-1])*(const[0] - const[1]*K[num][i][2])*K[num][i][1]
+	for i in range(st, S):
+		if math.fabs(math.sqrt(K_cash[i][0]**2 + K_cash[i][1]**2 + K_cash[i][2]**2) - math.sqrt(K[i][0]**2 + K[i][1]**2 + K[i][2]**2)) > eps:
+			res = res + float(b[i][en-1])*(const[0] - const[1]*K_cash[i][2])*K_cash[i][1]
+		else:
+			res = 0.0
 	return res
 	
 def sum2(st, en):
@@ -86,10 +90,13 @@ def sum2(st, en):
 		res = res + float(b[i][en-1])*(-const[2] + const[3]*K[i][1])*K[i][2]
 	return res
 
-def sum2_im(num, st, en):
+def sum2_im(st):
 	res = 0.0
-	for i in range(st, en):
-		res = res + float(b[i][en-1])*(-const[2] + const[3]*K[num][i][1])*K[num][i][2]
+	for i in range(st, S):
+		if math.fabs(math.sqrt(K_cash[i][0]**2 + K_cash[i][1]**2 + K_cash[i][2]**2) - math.sqrt(K[i][0]**2 + K[i][1]**2 + K[i][2]**2)) > eps:
+			res = res + float(b[i][en-1])*(-const[2] + const[3]*K[i][1])*K[i][2]
+		else:
+			res = 0.0		
 	return res
 
 def sum3():
@@ -128,23 +135,28 @@ def step(n, k): #calculate k and next y
 	y2.append(y2[n] + tau[k]*sum4())
 	
 def step_im(n, k): #calculate k and next y
-	K[n][0][0] = t[n] + float(a[0])
-	K[n][0][1] = y1[n]
-	K[n][0][2] = y2[n]
+	K[0][0] = t[n] + float(a[0])
+	K[0][1] = y1[n]
+	K[0][2] = y2[n]
 	for j in range(1, S):
-		K[n][j][0] = t[n] + float(a[j])
-		K[n][j][1] = y1[n] + tau[k]*sum1(n, 0, j)
-		K[n][j][2] = y2[n] + tau[k]*sum2(n, 0, j)
+		K[j][0] = t[n] + float(a[j])
+		K[j][1] = y1[n] + tau[k]*sum1_im(0)
+		K[j][2] = y2[n] + tau[k]*sum2_im(0)
+	K_cash = K.copy()
 	y1.append(y1[n] + tau[k]*sum3())
 	y2.append(y2[n] + tau[k]*sum4())
 
 
 
 set_data('input.dat')
-set_t(5, 40)
-for i in range(0, get_range(5, 40)):
-	step(i, 5)
-print_data(5)
+set_t(4, 40)
+if eps == 0.0:
+	for i in range(0, get_range(4, 40)):
+		step(i, 4)
+else:
+	for i in range(0, get_range(4, 40)):
+		step_im(i, 4)	
+print_data(4)
 
 
 
